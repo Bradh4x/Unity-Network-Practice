329dd34f64f32f224189e63496b5bd1eb8c605b4
Brandon Smith <yoshihiro2011@yahoo.com>
2021-06-02 06:18:19 -0400

    Now with UIs added

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour
+{
+    public void Hello()
+    {
+        Debug.Log("Hello");
+    }
+}
