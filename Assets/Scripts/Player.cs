a2a1a5b8f2ff3a15b7d431fcc35163f012d2dc7b
Brandon Smith <yoshihiro2011@yahoo.com>
2021-06-02 01:51:30 -0400

    Unity Multiplayer Edition Practice

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    void HandleMovement(){
+        if (isLocalPlayer)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+}
